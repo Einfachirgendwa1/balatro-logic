5bae2deb815858e62cfb34ede5fbe03c08f5983b
Einfachirgendwa1 <153438063+Einfachirgendwa1@users.noreply.github.com>
2026-03-01 01:40:43 +0100

    added a few tests

---
+++ tests/blinds.rs
@@ -0,0 +1,25 @@
+﻿use balatro_logic::{
+    blind::BossBlindType::{
+        TheFish, TheFlint, TheHouse, TheManacle, TheTooth, TheWall, TheWindow, VerdantLeaf,
+    },
+    builders::run::RunCreator,
+};
+
+#[test]
+fn boss_blinds() {
+    let mut run = RunCreator::builder().seed("AAAAAAAA".to_string()).build().create();
+    assert_eq!(run.data.this_antes_boss, TheManacle);
+
+    let mut next_boss_is = |boss| {
+        run.data.ante += 1;
+        assert_eq!(run.data.current_boss(), boss);
+    };
+
+    next_boss_is(TheHouse);
+    next_boss_is(TheFish);
+    next_boss_is(TheWindow);
+    next_boss_is(TheWall);
+    next_boss_is(TheTooth);
+    next_boss_is(TheFlint);
+    next_boss_is(VerdantLeaf);
+}
--- tests/shop_generation.rs
+++ tests/shop_generation.rs
@@ -1,53 +1,84 @@
 ﻿use balatro_logic::{
+    boosters::{
+        BoosterPackData,
+        BoosterPackType::{BuffoonNormal, CelestialJumbo},
+    },
     builders::run::RunCreator,
     consumable::{Consumable::TarotCard, Tarot},
-    decks::DeckType,
+    hands::HandType::{FourOfAKind, HighCard, Pair, ThreeOfAKind, TwoPair},
     joker::{Joker, JokerType},
     shop::ShopItem,
-    stake::Stake,
     vouchers::Voucher,
 };
 
 #[test]
 fn vouchers() {
-    let mut run = RunCreator::builder()
-        .deck(DeckType::Red)
-        .stake(Stake::White)
-        .seed("AAAAAAAA".to_string())
-        .build()
-        .create();
-
+    let mut run = RunCreator::builder().seed("AAAAAAAA".to_string()).build().create();
     run.data.ante = 0;
-    assert_eq!(run.data.poll_next_voucher(), Voucher::TarotMerchant);
-    run.data.ante += 1;
-    assert_eq!(run.data.poll_next_voucher(), Voucher::PlanetMerchant);
-    run.data.ante += 1;
-    assert_eq!(run.data.poll_next_voucher(), Voucher::MagicTrick);
-    run.data.ante += 1;
-    assert_eq!(run.data.poll_next_voucher(), Voucher::SeedMoney);
-    run.data.ante += 1;
-    assert_eq!(run.data.poll_next_voucher(), Voucher::Telescope);
-    run.data.ante += 1;
-    assert_eq!(run.data.poll_next_voucher(), Voucher::PaintBrush);
-    run.data.ante += 1;
-    assert_eq!(run.data.poll_next_voucher(), Voucher::SeedMoney);
-    run.data.ante += 1;
-    assert_eq!(run.data.poll_next_voucher(), Voucher::Hieroglyph);
-    run.data.ante += 1;
-    assert_eq!(run.data.poll_next_voucher(), Voucher::Blank);
 
-    run.data.ante = 1;
-    assert!(
-        matches!(run.poll_next_shop_item(), ShopItem::Joker(Joker { joker_type , .. }) if joker_type == JokerType::Bull)
-    );
-    assert!(
-        matches!(run.poll_next_shop_item(), ShopItem::Joker(Joker { joker_type , .. }) if joker_type == JokerType::FacelessJoker)
-    );
-    run.poll_next_shop_item();
-    assert_eq!(run.poll_next_shop_item(), ShopItem::Consumable(TarotCard(Tarot::TheSun)));
+    let mut next_voucher = |voucher| {
+        run.enter_shop(true);
+        run.data.ante += 1;
+
+        assert_eq!(run.data.shop.vouchers[0], voucher);
+    };
+
+    next_voucher(Voucher::TarotMerchant);
+    next_voucher(Voucher::PlanetMerchant);
+    next_voucher(Voucher::MagicTrick);
+    next_voucher(Voucher::SeedMoney);
+    next_voucher(Voucher::Telescope);
+    next_voucher(Voucher::PaintBrush);
+    next_voucher(Voucher::SeedMoney);
+    next_voucher(Voucher::Hieroglyph);
+    next_voucher(Voucher::Blank);
+}
+
+#[test]
+fn shop_inventory() {
+    fn is(shop_item: &ShopItem, j_type: JokerType) -> bool {
+        matches!(shop_item, ShopItem::Joker(Joker { joker_type , .. }) if joker_type == &j_type)
+    }
+    let mut run = RunCreator::builder().seed("AAAAAAAA".to_string()).build().create();
+    run.enter_shop(true);
+
+    assert!(is(&run.data.shop.inventory[0], JokerType::Bull));
+    assert!(is(&run.data.shop.inventory[1], JokerType::FacelessJoker));
+
+    run.reroll();
+    assert!(is(&run.data.shop.inventory[0], JokerType::Misprint));
+    assert_eq!(&run.data.shop.inventory[1], &ShopItem::Consumable(TarotCard(Tarot::TheSun)));
 
     run.data.ante = 2;
-    assert!(
-        matches!(run.poll_next_shop_item(), ShopItem::Joker(Joker { joker_type , .. }) if joker_type == JokerType::Bloodstone)
-    );
+    run.enter_shop(true);
+
+    assert!(is(&run.data.shop.inventory[0], JokerType::Bloodstone));
+}
+
+#[test]
+fn shop_packs() {
+    fn is(joker: &Joker, j_type: JokerType) -> bool {
+        matches!(joker, Joker { joker_type , .. } if joker_type == &j_type)
+    }
+
+    let mut run = RunCreator::builder().seed("AAAAAAAA".to_string()).build().create();
+    run.enter_shop(true);
+
+    assert_eq!(run.data.shop.packs, [Some(BuffoonNormal), Some(CelestialJumbo)]);
+
+    match run.open_booster_pack(BuffoonNormal) {
+        BoosterPackData::BuffoonNormal([a, b]) => {
+            assert!(is(&a, JokerType::ShootTheMoon));
+            assert!(is(&b, JokerType::ZanyJoker));
+        }
+        _ => unreachable!(),
+    }
+
+    let e = [ThreeOfAKind.into(), Pair.into(), FourOfAKind.into(), TwoPair.into(), HighCard.into()];
+    match run.open_booster_pack(CelestialJumbo) {
+        BoosterPackData::CelestialJumbo(list) => {
+            assert_eq!(list, e);
+        }
+        _ => unreachable!(),
+    }
 }
